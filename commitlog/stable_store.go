c0a90d8b143b785e76721013b74592bbd6eb98fc
Conor Mongey <conor@mongey.net>
2017-12-19 04:15:07 +0000

    Start Stable Store

---
+++ commitlog/stable_store.go
@@ -0,0 +1,17 @@
+package commitlog
+
+func (c *CommitLog) Set(key []byte, val []byte) error {
+	panic("not implemented")
+}
+
+func (c *CommitLog) Get(key []byte) ([]byte, error) {
+	panic("not implemented")
+}
+
+func (c *CommitLog) SetUint64(key []byte, val uint64) error {
+	panic("not implemented")
+}
+
+func (c *CommitLog) GetUint64(key []byte) (uint64, error) {
+	panic("not implemented")
+}
